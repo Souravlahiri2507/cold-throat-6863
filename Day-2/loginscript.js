b844aeb982f3d61e2a28bd827d1a0fc8679fd04d
Sourav Lahiri <cesouravlahiri4098@gmail.com>
2022-11-10 23:48:08 +0530

    day3_sproject

---
+++ Day-2/loginscript.js
@@ -0,0 +1,4 @@
+    document.getElementById("login-button").addEventListener("click",myFun);
+    function myFun(){
+        alert(document.getElementById("login-email").value+" "+"Successfully Registered");
+    }
