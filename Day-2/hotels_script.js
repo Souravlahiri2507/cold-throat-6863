50851b9ee37b910ead2ed14278787a8f4c1e2587
Sourav Lahiri <cesouravlahiri4098@gmail.com>
2022-11-11 22:52:37 +0530

    day-4_project_submission

---
+++ Day-2/hotels_script.js
@@ -0,0 +1,8 @@
+let check_alert = document.querySelectorAll(".hotel-rating>a>button");
+for(let i=0;i<check_alert.length;i++){
+    check_alert[i].addEventListener("click",myFun);
+}
+function myFun(){
+    alert("You are moving towards the Checkout Page");
+}
+
