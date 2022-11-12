ba2fa82473ca27624f9d569f445f5d316526cbef
Sourav Lahiri <cesouravlahiri4098@gmail.com>
2022-11-12 23:11:41 +0530

    day-5_project_submission

--- Day-2/loginscript.js
+++ Day-2/loginscript.js
@@ -1,4 +1,12 @@
+    let emailArr = JSON.parse(localStorage.getItem("register_data"))||[];
     document.getElementById("login-button").addEventListener("click",myFun);
     function myFun(){
         alert(document.getElementById("login-email").value+" "+"Successfully Registered");
+
+        let mail = document.querySelector("#login-email").value;
+        emailArr.push(mail);
+        console.log(emailArr);
+
+        localStorage.setItem("register_data",JSON.stringify(emailArr));
     }
+
